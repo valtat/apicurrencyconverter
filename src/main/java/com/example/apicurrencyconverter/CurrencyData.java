3dd1e20dba12a50d4887a88436c232b905efc9c8
Valerio Tatananni <valerio.tatananni@gmail.com>
2024-09-18 17:23:17 +0300

    Refactor CurrencyData class for MongoDB integration

--- src/main/java/com/example/apicurrencyconverter/CurrencyData.java
+++ src/main/java/com/example/apicurrencyconverter/CurrencyData.java
@@ -1,11 +1,78 @@
 package com.example.apicurrencyconverter;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.util.Map;
 
-public record CurrencyData(boolean success, long timestamp, String base, String date, Map<String, Double> rates) {
+@Document(collection = "currencyData")
+public class CurrencyData {
 
-    public CurrencyData() {
-        this(false, 0, "", "", Map.of());
+    @Id
+    private String id;
+    private boolean success;
+    private long timestamp;
+    private String base;
+    private String date;
+    private Map<String, Double> rates;
+
+    public String getId() {
+        return id;
     }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getBase() {
+        return base;
+    }
+
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Map<String, Double> getRates() {
+        return rates;
+    }
+
+    public void setRates(Map<String, Double> rates) {
+        this.rates = rates;
+    }
+
+    @Override
+    public String toString() {
+        return "CurrencyData{" +
+                "id='" + id + '\'' +
+                ", success=" + success +
+                ", timestamp=" + timestamp +
+                ", base='" + base + '\'' +
+                ", date='" + date + '\'' +
+                ", rates=" + rates +
+                '}';
+    }
 }

4dc9d1822e247b9e504582ed868b03826eebd7e5
Valerio Tatananni <valerio.tatananni@gmail.com>
2024-09-18 17:23:49 +0300

    Refactor ApicurrencyconverterApplication to enable scheduling

--- src/main/java/com/example/apicurrencyconverter/ApicurrencyconverterApplication.java
+++ src/main/java/com/example/apicurrencyconverter/ApicurrencyconverterApplication.java
@@ -2,9 +2,11 @@ package com.example.apicurrencyconverter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
+@EnableScheduling
 public class ApicurrencyconverterApplication {
 
 	public static void main(String[] args) {

dd3811a6b5e4df8b89fcd9be2a93ffe06ddd6d2e
Valerio Tatananni <valerio.tatananni@gmail.com>
2024-09-18 17:23:36 +0300

    Refactor CurrencyDataController to use CurrencyDataService for fetching and storing currency data

--- src/main/java/com/example/apicurrencyconverter/CurrencyDataController.java
+++ src/main/java/com/example/apicurrencyconverter/CurrencyDataController.java
@@ -1,26 +1,27 @@
 package com.example.apicurrencyconverter;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @CrossOrigin(origins = {"http://localhost:5173", "https://valeriot.z16.web.core.windows.net/"})
 public class CurrencyDataController {
 
-    @Value("${EXCHANGE_RATES_API_KEY:defaultApiKey}")
-    private String apiKey;
+    private final CurrencyDataService currencyDataService;
 
-    private static final String BASE_URL = "http://api.exchangeratesapi.io/v1/";
+    public CurrencyDataController(CurrencyDataService currencyDataService) {
+        this.currencyDataService = currencyDataService;
+    }
 
     @GetMapping("/api/currency")
     public CurrencyData getCurrencyData() {
-        String endpoint = "latest";
-        String url = BASE_URL + endpoint + "?access_key=" + apiKey;
-        RestTemplate restTemplate = new RestTemplate();
-        CurrencyData currencyData = restTemplate.getForObject(url, CurrencyData.class);
-        return currencyData != null ? currencyData : new CurrencyData();
+        return currencyDataService.getLatestCurrencyData();
+    }
+
+    @GetMapping("/api/change-currency")
+    public double changeCurrency(@RequestParam double amount, @RequestParam String from, @RequestParam String to) {
+        return currencyDataService.convertCurrency(amount, from, to);
     }
 }

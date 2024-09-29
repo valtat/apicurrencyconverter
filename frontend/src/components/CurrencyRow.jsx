5861f88addb4c892cb4b223235fb61d8437ccfa7
valtat <82716180+valtat@users.noreply.github.com>
2024-09-29 18:14:00 +0300

    Refactor CurrencyRow component to include separate select elements for "From" and "To" currencies, and add labels for input and select elements

--- frontend/src/components/CurrencyRow.jsx
+++ frontend/src/components/CurrencyRow.jsx
@@ -2,30 +2,59 @@ import React from "react";
 
 export default function CurrencyRow({
   currencyOptions,
-  selectedCurrency,
-  onChangeCurrency,
+  selectedCurrency1,
+  onChangeCurrency1,
+  selectedCurrency2,
+  onChangeCurrency2,
   onChangeAmount,
   amount,
 }) {
   return (
-    <div>
-      <input
-        type="number"
-        className="input"
-        value={amount}
-        onChange={onChangeAmount}
-      />
-      <select
-        className="value-box"
-        value={selectedCurrency}
-        onChange={onChangeCurrency}
-      >
-        {currencyOptions.map((option) => (
-          <option key={option} value={option}>
-            {option}
-          </option>
-        ))}
-      </select>
+    <div className="row">
+      <div className="input-container">
+        <label htmlFor="amountInput">Amount:</label>
+        <input
+          type="number"
+          className="input"
+          value={amount}
+          onChange={onChangeAmount}
+          id="amountInput"
+        />
+      </div>
+
+      <div className="select-container">
+        <div>
+          <label htmlFor="fromCurrencySelect">From:</label>
+          <select
+            className="value-box"
+            value={selectedCurrency1}
+            onChange={onChangeCurrency1}
+            id="fromCurrencySelect"
+          >
+            {currencyOptions.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div>
+          <label htmlFor="toCurrencySelect">To:</label>
+          <select
+            className="value-box"
+            value={selectedCurrency2}
+            onChange={onChangeCurrency2}
+            id="toCurrencySelect"
+          >
+            {currencyOptions.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
     </div>
   );
 }

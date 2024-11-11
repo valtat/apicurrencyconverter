7bfebfc3c1c0119db1821a84a6b97f139b175b7a
valtat <82716180+valtat@users.noreply.github.com>
2024-11-11 20:12:58 +0200

    Refactor useFetch hook to fetch currency data from BASE_URL

--- frontend/src/hooks/useFetch.jsx
+++ frontend/src/hooks/useFetch.jsx
@@ -1,36 +1,37 @@
 import { useState, useEffect } from "react";
 
 const useFetch = (BASE_URL) => {
-  const [currencies, setCurrencies] = useState([]);
+  const [, setCurrencies] = useState([]);
   const [timestamp, setTimestamp] = useState("");
   const [currencyOptions, setCurrencyOptions] = useState([]);
   const [fromCurrency, setFromCurrency] = useState("");
   const [toCurrency, setToCurrency] = useState("");
-  const [initialRate, setInitialRate] = useState(0);
+  const [amount, setAmount] = useState(1);
 
   useEffect(() => {
     fetch(BASE_URL)
       .then((res) => res.json())
       .then((data) => {
-        setCurrencies(data);
+        setCurrencies(Object.keys(data.rates));
         setTimestamp(data.timestamp);
         const initialCurrency = Object.keys(data.rates)[0];
         setCurrencyOptions([data.base, ...Object.keys(data.rates)]);
         setFromCurrency(data.base);
         setToCurrency(initialCurrency);
-        setInitialRate(data.rates[initialCurrency]);
-      });
+        setAmount(1);
+      })
+      .catch((error) => console.error("Error fetching currencies:", error));
   }, [BASE_URL]);
 
   return {
-    currencies,
     timestamp,
     currencyOptions,
     fromCurrency,
     toCurrency,
     setFromCurrency,
     setToCurrency,
-    initialRate,
+    amount,
+    setAmount,
   };
 };
 

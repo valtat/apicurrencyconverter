06e76e1b77c003c47ef34af4ccd8caf089f00077
valtat <82716180+valtat@users.noreply.github.com>
2024-11-11 20:11:50 +0200

    Refactor App.jsx to use custom hooks useFetch and useCurrencyConverter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import CurrencyConverterText from "./components/CurrencyConverterText.jsx";
 import TimeStamp from "./components/TimeStamp.jsx";
 import ConvertButton from "./components/ConvertButton.jsx";
 import useFetch from "./hooks/useFetch.jsx";
-import React, { useState, useEffect } from "react";
+import useCurrencyConverter from "./hooks/useCurrencyConverter";
 import ResultText from "./components/ResultText.jsx";
 
 const BASE_URL = "http://localhost:8080/api/currency";
@@ -14,47 +14,24 @@ const CONVERT_URL = "http://localhost:8080/api/change-currency";
 //"https://apicurrencyconverter-hndhcrcaczayhudr.northeurope-01.azurewebsites.net/api/change-currency"
 
 function App() {
-  const [currencies, setCurrencies] = useState([]);
-  const [fromCurrency, setFromCurrency] = useState("");
-  const [toCurrency, setToCurrency] = useState("");
+  const {
+    timestamp,
+    currencyOptions,
+    fromCurrency,
+    toCurrency,
+    setFromCurrency,
+    setToCurrency,
+    amount,
+    setAmount,
+  } = useFetch(BASE_URL);
 
-  const [conversionAmount, setConversionAmount] = useState("");
-  const [conversionFromCurrency, setConversionFromCurrency] = useState("");
-  const [conversionToCurrency, setConversionToCurrency] = useState("");
-
-  const [amount, setAmount] = useState("");
-  const [convertedAmount, setConvertedAmount] = useState("");
-
-  const { timestamp, currencyOptions } = useFetch(BASE_URL);
-
-  useEffect(() => {
-    fetch(BASE_URL)
-      .then((res) => res.json())
-      .then((data) => {
-        setCurrencies(Object.keys(data.rates));
-        setFromCurrency(data.base);
-        setToCurrency(Object.keys(data.rates)[0]);
-        setAmount(1);
-      })
-      .catch((error) => console.error("Error fetching currencies:", error));
-  }, []);
-
-  const handleConvert = () => {
-    if (amount && fromCurrency && toCurrency) {
-      const url = `${CONVERT_URL}?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`;
-      fetch(url)
-        .then((res) => res.json())
-        .then((data) => {
-          setConvertedAmount(data);
-        })
-        .catch((error) => console.error("Error converting currency:", error));
-      setConversionAmount(amount);
-      setConversionFromCurrency(fromCurrency);
-      setConversionToCurrency(toCurrency);
-    } else {
-      console.error("Amount, fromCurrency, and toCurrency must be provided");
-    }
-  };
+  const {
+    handleConvert,
+    convertedAmount,
+    conversionAmount,
+    conversionFromCurrency,
+    conversionToCurrency,
+  } = useCurrencyConverter(fromCurrency, toCurrency, CONVERT_URL, amount);
 
   function handleFromAmountChange(e) {
     const newValue = e.target.value.replace(/[^0-9.]/g, "");

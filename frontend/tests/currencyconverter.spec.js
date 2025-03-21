f087ddb614570609a14364e8c9f27bacbd3f1561
Valerio Tatananni <valerio.tatananni@gmail.com>
2025-03-21 12:31:50 +0200

    Changed URL to Azure Static Web App URL

--- frontend/tests/currencyconverter.spec.js
+++ frontend/tests/currencyconverter.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("should be able to choose a currency and convert it", async ({ page }) => {
-  await page.goto("http://localhost:5173/");
+  await page.goto("https://kind-island-02bf6e003.6.azurestaticapps.net/");
   await page.getByLabel("Amount:").click();
   await page.getByLabel("Amount:").fill("110");
   await page.getByLabel("To:").selectOption("USD");
